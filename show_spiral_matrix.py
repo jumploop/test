6e0641aadda0e77af0976e4ad1d8adc512fc65b7
liming <827182486@qq.com>
2022-03-06 17:50:20 +0800

    简单测试代码

---
+++ show_spiral_matrix.py
@@ -0,0 +1,37 @@
+def show_spiral_matrix(n):
+    matrix = [[0] * n for _ in range(n)]
+    row, col = 0, 0
+    num, direction = 1, 0
+    while num <= n**2:
+        if matrix[row][col] == 0:
+            matrix[row][col] = num
+            num += 1
+        if direction == 0:
+            if col < n - 1 and matrix[row][col + 1] == 0:
+                col += 1
+            else:
+                direction += 1
+        elif direction == 1:
+            if row < n - 1 and matrix[row + 1][col] == 0:
+                row += 1
+            else:
+                direction += 1
+        elif direction == 2:
+            if col > 0 and matrix[row][col - 1] == 0:
+                col -= 1
+            else:
+                direction += 1
+        else:
+            if row > 0 and matrix[row - 1][col] == 0:
+                row -= 1
+            else:
+                direction += 1
+        direction %= 4
+    for x in matrix:
+        for y in x:
+            print(y, end='\t')
+        print()
+
+
+if __name__ == '__main__':
+    show_spiral_matrix(10)
